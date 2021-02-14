e26e8a271de7ef546fe01b36e2936c11ffd8b302
Alex Gleyzer <alex@gleyzer.com>
2021-02-14 17:07:12 +0000

    Simple counter

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+  "strconv"
+  
+  "github.com/replit/database-go"
+)
+
+const key = "default"
+
+func count(w http.ResponseWriter, req *http.Request) {
+  value, err := database.Get(key)
+  if err == database.ErrNotFound {
+    value = "0"
+    err = nil
+  }
+  if err != nil {
+    http.Error(w, fmt.Sprintf("Database get error: %v", err), http.StatusInternalServerError)
+    return
+  }
+
+  count, err := strconv.Atoi(value)
+  if err != nil {
+    http.Error(w, fmt.Sprintf("Atoi error: %v", err), http.StatusInternalServerError)
+    return
+  }
+
+  count = count + 1 
+
+  if err := database.Set(key, strconv.Itoa(count)); err != nil {
+    http.Error(w, fmt.Sprintf("Database set error: %v", err), http.StatusInternalServerError)
+    return
+  }
+
+  fmt.Fprintf(w, `{"count": %d}`, count)
+}
 
 func main() {
-  fmt.Println("Hello World")
+	http.HandleFunc("/", count)  
+	http.ListenAndServe(":8080", nil)
 }
